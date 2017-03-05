75cb82137a009b7ec8015d7bacc8dde4293feaa4
Araknus <rexlampart@gmail.com>
2017-03-05 14:53:19 +0100

    01.1

---
+++ 01.1/01.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Samochod> samochody = new List<Samochod>
+            {
+            new Samochod("Opel Astra", 4, 80, "Biały", 2015),
+            new Samochod("Toyota Corolla", 4, 90, "Srebrna", 2004),
+            new Samochod("VW Golf 4", 4, 70, "Czerwony", 2002)
+            };
+            samochody[1].Hamuj();
+            foreach ( var ten in samochody)
+            {
+                samochody[1].Jedz(90);
+
+
+                Console.WriteLine("{0} samochód marki {1} jedzie z prędkością {2}",ten.Kolor,ten.Marka,ten.Predkosc);
+                Console.WriteLine("Domyślna ilość kół używanych jednocześnie w samochodzie to {0}", ten.LiczbkaKol);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ 01.1/01.1/Samochod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._1
+{
+    class Samochod
+    {
+        public string Marka { get; private set; }
+        public int LiczbkaKol { get; private set; }
+        public int Predkosc { get; private set; }
+        public string Kolor { get; private set; }
+        public int Rocznik { get; private set; }
+
+        public Samochod(string marka,int liczbaKol, int predkosc, string kolor = "Czarny",int rocznik = 2016 )
+        {
+            Marka = marka;
+            LiczbkaKol = liczbaKol;
+            Predkosc = predkosc;
+            Kolor = kolor;
+            Rocznik = rocznik;
+        }
+
+        public void Jedz(int v1)
+        {
+            Predkosc = v1;
+        }
+
+        public void Hamuj ()
+        {
+            Predkosc = 0;
+           
+
+
+        }
+    }
+}

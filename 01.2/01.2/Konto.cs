b6613a983b48352e8bfe2b693647ea9e943d1949
Araknus <rexlampart@gmail.com>
2017-03-06 20:51:25 +0100

    Add files via upload

---
+++ 01.2/01.2/Konto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._2
+{
+    class Konto
+    {
+        private string Imie { get; set; }
+        private string Nazwisko { get; set; }
+        private int NumerKonta { get; set; }
+        private int Fundusze { get; set; }
+        private int WewId { get; set; }
+        private bool DoUsuniecia { get; set; }
+
+
+        public Konto(string imie,string nazwisko,int numerKonta,int fundusze, int wewId, bool doUsuniecia=false)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            NumerKonta = numerKonta;
+            Fundusze = fundusze;
+            WewId = wewId;
+            DoUsuniecia = doUsuniecia;
+
+
+        }
+
+        public void WpłaćPieniądze(int ilosc)
+        {
+            Fundusze += ilosc;
+        }
+
+        public void WypłaćPieniądze(int ilosc)
+        {
+            Fundusze -= ilosc;
+        }
+
+        public void UsuńKonto(List<Konto> lista, int id)
+        {
+            lista.RemoveAt(id);
+        }
+
+        public void ZmieńStatusDoUsunięcia(bool status)
+        {
+            status = true;
+        }
+
+
+        public void ZmieńFunduszeKonta(int procent)
+        {
+            Fundusze += Fundusze*procent/100;
+        }
+
+        public override string ToString()
+        {
+            return  "Użytkownik konta " + Imie.ToString() +" " + Nazwisko.ToString() + " ma " + Fundusze.ToString() +" zł na koncie";
+        }
+
+    }
+}
---
+++ 01.2/01.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Konto> konta = new List<Konto>
+            {
+            new Konto("Jan", "Kowalski", 11142, 900, 1),
+            new Konto("Zbigniew", "Bilski", 68921, 1800, 2),
+            new Konto("Maciej", "Urbanowicz", 56181, 2600, 3)
+            };
+            konta[0].WpłaćPieniądze(150);
+            konta[1].ZmieńFunduszeKonta(5);
+            konta[2].ZmieńStatusDoUsunięcia(false);
+
+            Console.WriteLine(konta[0].ToString());
+            Console.WriteLine(konta[1].ToString());
+            Console.ReadKey();
+            
+            
+        }
+    }
+}

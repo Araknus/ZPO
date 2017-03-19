129ae4bf1f6ac485be5b4a186d0e7ae2ec673a67
Araknus <rexlampart@gmail.com>
2017-03-19 14:11:06 +0100

    Add files via upload

---
+++ 3 ćwiczenia/03.1/Dzielenie.Tests/DzielenieTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dzielenie.Tests
+{
+    [TestFixture]
+    public class DzielenieTests
+    {
+      
+        [Test]
+        public void Dzielenie_2liczb()
+        {
+            var calc = new Dzielenie();
+            double wynik = calc.DivideNumbers(2, 2);
+
+            Assert.AreEqual(1, wynik);
+            Assert.AreEqual(4, calc.DivideNumbers(8, 2));
+            
+        }
+    }
+}
---
+++ 3 ćwiczenia/03.1/Dzielenie/Dzielenie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dzielenie
+{
+    public class Dzielenie
+    {
+        public Double DivideNumbers(double a, double b)
+        {
+            if (b == 0)
+                throw new System.DivideByZeroException();
+            return a / b;
+        }
+    }
+}

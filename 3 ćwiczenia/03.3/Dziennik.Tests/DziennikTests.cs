24911cf4b4481f9ae5c7e92531e51f7cfb2ce500
Maciej Lamperski <rexlampart@gmail.com>
2017-03-20 21:33:21 +0100

    dodanie klasy z testami

--- 3 ćwiczenia/03.3/Dziennik.Tests/DziennikTests.cs
+++ 3 ćwiczenia/03.3/Dziennik.Tests/DziennikTests.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Dziennik.Tests
 {
+    [TestFixture]
     public class DziennikTests
     {
+        [Test]
+        public void Dziennik()
+        {
+
+
+
+        }
+
     }
 }
